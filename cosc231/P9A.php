ed4a5e30696668c869d4c36f768dc8da2555185d
Brendan Klein <deliveritthere@gmail.com>
2020-11-02 13:22:26 -0500

    Updated code to Reflect requirments of Project 9 rubric

--- cosc231/P9A.php
+++ cosc231/P9A.php
@@ -4,9 +4,12 @@
         <title>Dice Rolling Simulation</title>
     </head>
     <body>
-        <h1>Dice Rolling Simulator</h1>
+        
     
     <?php 
+        echo "<h1>Dice Rolling Simulator</h1>";
+
+
         echo "<p>This program simulates rolling dice between 50, and 500 times.</p> ";
         echo "<p>It then adds up the sum of all of the 1s 2s 3s 4s 5s and 6s.</p>";
 
@@ -17,41 +20,42 @@
         $fivecount =0;
         $sixcount =0;
 
-    $arr=[];
-    function dicegen($a, $b)
-    {
-    	return rand($a,$b);
-    }
+        $arr=[];
+        function dicegen($a, $b)
+        {
+    	    return rand($a,$b);
+        }
 
-    $u=dicegen(50,500);
-    echo "Total number of rolls: " . $u."<br><br>";
-    for($i=0; $i < $u; $i++)
-    {
-        $selector = dicegen(1,6);
+        $u=dicegen(50,500);
+        echo "Total number of rolls: " . $u."<br><br>";
+        
+        for($i=0; $i < $u; $i++)
+        {
+            $selector = dicegen(1,6);
 
-        if($selector==1)
-            $onecount=$onecount+1;
-        if($selector==2)
-            $twocount=$twocount+1;
-        if($selector==3)
-            $threecount=$threecount+1;
-        if($selector==4)
-            $fourcount=$fourcount+1;
-        if($selector==5)
-            $fivecount=$fivecount+1;
-        if($selector==6)
-            $sixcount=$sixcount+1;
+            if($selector==1)
+                $onecount=$onecount+1;
+            if($selector==2)
+                $twocount=$twocount+1;
+            if($selector==3)
+                $threecount=$threecount+1;
+            if($selector==4)
+                $fourcount=$fourcount+1;
+            if($selector==5)
+                $fivecount=$fivecount+1;
+            if($selector==6)
+                $sixcount=$sixcount+1;
 
-        array_push($arr,dicegen(1,6));
-        echo "Roll " .($i+1).": ". $arr[$i] . "<br>";
-    }
-    echo "======================== <br>";
-    echo "Number of 1s rolled: ".$onecount ."<br>";
-    echo "Number of 2s rolled: ".$twocount ."<br>";
-    echo "Number of 3s rolled: ".$threecount ."<br>";
-    echo "Number of 4s rolled: ".$fourcount ."<br>";
-    echo "Number of 5s rolled: ".$fivecount ."<br>";
-    echo "Number of 6s rolled: ".$sixcount ."<br>";
+            array_push($arr,dicegen(1,6));
+            echo "Roll " .($i+1).": ". $arr[$i] . "<br>";
+        }
+        echo "======================== <br>";
+        echo "Number of 1s rolled: ".$onecount ."<br>";
+        echo "Number of 2s rolled: ".$twocount ."<br>";
+        echo "Number of 3s rolled: ".$threecount ."<br>";
+        echo "Number of 4s rolled: ".$fourcount ."<br>";
+        echo "Number of 5s rolled: ".$fivecount ."<br>";
+        echo "Number of 6s rolled: ".$sixcount ."<br>";
 
     ?>
     </body>
--- cosc231/P9B.php
+++ cosc231/P9B.php
@@ -4,29 +4,38 @@
         <title>Coin Flip Simulation</title>
     </head>
     <body>
-        <h1>Coin Flip Simulation</h1>
-    <?php 
-    
-    $arr=[];
-    function coinflip($a,$b)
-    {
-        $x = rand($a,$b);
-        if($x == 1)   //Converts a 1 to heads
-            return "Heads";
-        if($x == 2)   //Converts a 2 to tails 
-            return "Tails";
-        if($x > 99)   //Returns a number generated between 100 and 500 without using a seperate function
-            return $x;
-    }
-    $u=coinflip(100,500); //Randomly generates number of times coin will be flipped
-    echo "Total Number of flipped coins: ".$u . "<br>";
-    
-    for($i=0; $i < $u; $i++)
-    {
-        array_push($arr,coinflip(1,6));
-        echo "Roll " .($i+1).": ". $arr[$i] . "<br>";
-    }
+        <?php 
+            echo "<h1>Coin Flip Simulation</h1>";
+            echo"<p>This program will flip a coin a specific number of times and display a summary flips, and the totaled output of the flips at the bottom of the page.</p><br>";
 
-    ?>
+            $arr=[];
+            function coinflip($a,$b)
+            {
+                $x = rand($a,$b);
+                if($x == 1)
+                    return "Heads";
+                if($x == 2)
+                    return "Tails";
+                if($x > 99)
+                    return $x;
+            }
+            $u=coinflip(100,500);
+            echo "Number of Heads and Tails Generated: ".$u . "<br>";
+            for($i=0; $i < $u; $i++)
+            {
+                $selector=coinflip(1,2);
+                array_push($arr,$selector);
+                echo "Flip ".($i+1).": ".$arr[$i]. "<br>";
+
+                if($selector=="Heads")
+                    $HeadsCount=$HeadsCount+1;
+                if($selector=="Tails")
+                    $TailsCount=$TailsCount+1;
+            }
+
+            echo "========================= <br>";
+            echo "Total Heads Count: ".$HeadsCount." <br>";
+            echo "Total Tails Count: ".$TailsCount;
+        ?>
     </body>
 </html>
